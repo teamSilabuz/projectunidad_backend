debc504c8982adddf9da79f786123d481b6cfa11
deyvisnvg <deyvisnvg@gmail.com>
2023-02-01 23:35:20 -0500

    Envío de contraseña por email, implementado

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,5 +6,6 @@ export default {
     DATABASE_URL: process.env.DATABASE_URL as string,
     PORT: process.env.PORT as string,
     TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID as string,
-    TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN as string
+    TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN as string,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY as string
 }
